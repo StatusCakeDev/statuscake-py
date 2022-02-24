d12de99bdb47a6390c6cfd10a6b45239767bf9d2
Tomas Basham <tomasbasham@gmail.com>
2022-02-24 22:20:25 +0000

    Add StatusCake client code

---
+++ statuscake/apis/__init__.py
@@ -0,0 +1,22 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.contact_groups_api import ContactGroupsApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from statuscake.api.contact_groups_api import ContactGroupsApi
+from statuscake.api.locations_api import LocationsApi
+from statuscake.api.maintenance_windows_api import MaintenanceWindowsApi
+from statuscake.api.pagespeed_api import PagespeedApi
+from statuscake.api.ssl_api import SslApi
+from statuscake.api.uptime_api import UptimeApi
---
+++ statuscake/models/__init__.py
@@ -0,0 +1,60 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from statuscake.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from statuscake.model.api_error import APIError
+from statuscake.model.api_response import APIResponse
+from statuscake.model.api_response_data import APIResponseData
+from statuscake.model.contact_group import ContactGroup
+from statuscake.model.contact_group_response import ContactGroupResponse
+from statuscake.model.contact_groups import ContactGroups
+from statuscake.model.maintenance_window import MaintenanceWindow
+from statuscake.model.maintenance_window_repeat_interval import MaintenanceWindowRepeatInterval
+from statuscake.model.maintenance_window_response import MaintenanceWindowResponse
+from statuscake.model.maintenance_window_state import MaintenanceWindowState
+from statuscake.model.maintenance_windows import MaintenanceWindows
+from statuscake.model.metadata import Metadata
+from statuscake.model.monitoring_location import MonitoringLocation
+from statuscake.model.monitoring_location_status import MonitoringLocationStatus
+from statuscake.model.monitoring_locations import MonitoringLocations
+from statuscake.model.pagespeed_test import PagespeedTest
+from statuscake.model.pagespeed_test_check_rate import PagespeedTestCheckRate
+from statuscake.model.pagespeed_test_history import PagespeedTestHistory
+from statuscake.model.pagespeed_test_history_data import PagespeedTestHistoryData
+from statuscake.model.pagespeed_test_history_data_aggregated import PagespeedTestHistoryDataAggregated
+from statuscake.model.pagespeed_test_history_data_aggregated_filesize import PagespeedTestHistoryDataAggregatedFilesize
+from statuscake.model.pagespeed_test_history_data_aggregated_loadtime import PagespeedTestHistoryDataAggregatedLoadtime
+from statuscake.model.pagespeed_test_history_data_aggregated_requests import PagespeedTestHistoryDataAggregatedRequests
+from statuscake.model.pagespeed_test_region import PagespeedTestRegion
+from statuscake.model.pagespeed_test_history_result import PagespeedTestHistoryResult
+from statuscake.model.pagespeed_test_response import PagespeedTestResponse
+from statuscake.model.pagespeed_test_stats import PagespeedTestStats
+from statuscake.model.pagespeed_test_throttling import PagespeedTestThrottling
+from statuscake.model.pagespeed_tests import PagespeedTests
+from statuscake.model.ssl_test import SSLTest
+from statuscake.model.ssl_test_check_rate import SSLTestCheckRate
+from statuscake.model.ssl_test_flags import SSLTestFlags
+from statuscake.model.ssl_test_mixed_content import SSLTestMixedContent
+from statuscake.model.ssl_test_response import SSLTestResponse
+from statuscake.model.ssl_tests import SSLTests
+from statuscake.model.uptime_test import UptimeTest
+from statuscake.model.uptime_test_alert import UptimeTestAlert
+from statuscake.model.uptime_test_alerts import UptimeTestAlerts
+from statuscake.model.uptime_test_check_rate import UptimeTestCheckRate
+from statuscake.model.uptime_test_history import UptimeTestHistory
+from statuscake.model.uptime_test_history_result import UptimeTestHistoryResult
+from statuscake.model.uptime_test_overview import UptimeTestOverview
+from statuscake.model.uptime_test_period import UptimeTestPeriod
+from statuscake.model.uptime_test_periods import UptimeTestPeriods
+from statuscake.model.uptime_test_processing_state import UptimeTestProcessingState
+from statuscake.model.uptime_test_response import UptimeTestResponse
+from statuscake.model.uptime_test_status import UptimeTestStatus
+from statuscake.model.uptime_test_type import UptimeTestType
+from statuscake.model.uptime_tests import UptimeTests
